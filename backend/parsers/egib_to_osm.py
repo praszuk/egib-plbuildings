6f62e42813998de9a8fcb7141f68434e7048e394
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-03-30 18:22:48 +0200

    Add egib_to_osm parser main function

---
+++ backend/parsers/egib_to_osm.py
@@ -0,0 +1,26 @@
+from typing import Any, Dict, Final
+
+from exceptions import ParserNotFound
+from .parser_epodgik import epodgik_parser
+
+
+TERYT_PARSER: Final = {
+    '1421': epodgik_parser,
+}
+
+
+def egib_to_osm(geojson: Dict[str, Any], teryt: str) -> None:
+    """
+    Change EGiB properties to OSM tags
+    :param geojson: raw EGIB GeoJSON from one wfs service/powiat
+    :param teryt: 4 numbers powiat (county) code as str
+    :raises ParserNotFound, InvalidKeyParserError, ParserError:
+    """
+    try:
+        parser = TERYT_PARSER[teryt]
+    except KeyError:
+        raise ParserNotFound(teryt)
+
+    for index, feature in enumerate(geojson['features']):
+        osm_tags = parser(feature['properties'])
+        geojson['features'][index]['properties'] = osm_tags
