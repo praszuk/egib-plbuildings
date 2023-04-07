9ac5bd461a7c47d1dda1478d3171923c9c48a7d0
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-04-07 13:41:45 +0200

    Add text client fixture for integration tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,20 @@
 import pytest
 
+from fastapi.testclient import TestClient
+
 from os import path
+
+from core.config import settings
+from main import app
 
 
 @pytest.fixture(scope='session')
 def test_data_dir():
     return path.join(path.dirname(__file__), 'data')
+
+
+@pytest.fixture(name='client', scope='session')
+def test_client():
+    client = TestClient(app)
+    client.base_url = client.base_url.join(settings.API_V1_STR)
+    yield client
