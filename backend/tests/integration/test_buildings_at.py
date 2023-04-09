4c916bdeb90af9e7cf7a0ce5e01966eaca539a0c
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-04-09 20:01:34 +0200

    Add integration test for simple building
    it uses async_client and monkeypatching
    it requires to refactor builidngs service to create _download_gml func

--- backend/tests/integration/test_buildings_at.py
+++ backend/tests/integration/test_buildings_at.py
@@ -1,3 +1,13 @@
+import httpx
+import pytest
+
+from os import path
+
+from main import app
+from core.config import settings
+from services import buildings
+
+
 def test_invalid_lat_lon(client):
     response = client.get('buildings/', params={'lat': 91.0, 'lon': 50.0})
     assert response.status_code == 422
@@ -8,3 +18,43 @@ def test_invalid_lat_lon(client):
     response = client.get('buildings/', params={'lat': 91.0, 'lon': 181.0})
     assert response.status_code == 422
 
+
+@pytest.fixture(name='async_client')
+async def test_async_client(test_data_dir):
+    async with httpx.AsyncClient(app=app, base_url='http://test') as client:
+        client.base_url = client.base_url.join(settings.API_V1_STR)
+        yield client
+
+
+async def mock_download_gml(test_data_dir, filename: str) -> str:
+    """
+    Override _download_gml from services buildings to return gml content
+    from file
+    :return: gml_content as string
+    """
+    filename = path.join(test_data_dir, filename)
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+@pytest.mark.anyio
+async def test_simple_building_data(async_client, monkeypatch, test_data_dir):
+    monkeypatch.setattr(
+        buildings,
+        '_download_gml',
+        lambda client, url: mock_download_gml(
+            test_data_dir,
+            'gml_basic_building.xml'
+        )
+    )
+
+    response = await async_client.get(
+        'buildings/',
+        params={'lat': 50, 'lon': 20}
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data['features']) == 1
+    assert 'building' in data['features'][0]['properties']
