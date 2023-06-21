b8acf188e004a100758da4c461d4f79b2062de81
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-06-21 23:13:33 +0200

    Fix mypy errors

--- backend/powiats/urls.py
+++ backend/powiats/urls.py
@@ -5,7 +5,7 @@ _SRSNAME = 'EPSG:4326'
 
 def epodgik_url(
     powiat: Powiat, lat: float, lon: float, srsname: str = _SRSNAME
-):
+) -> str:
     if not (area_name := powiat.url_extras.get('area_name', None)):
         raise ValueError('Missing area_name for epodgik url builder!')
 
