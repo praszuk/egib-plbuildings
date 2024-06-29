e980ca0366863b0a49598dfc7bc86a1fb9cac132
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-06-29 02:30:20 +0200

    Add simple status page
    It displays county svg map and highlights green/red counties depends on
    hc status

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,13 @@ from fastapi.staticfiles import StaticFiles
 from backend.api.v1.api import api_router
 from backend.core.config import settings
 from backend.areas.finder import area_finder
+from backend.pages.pages import pages_router
 
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory=settings.STATIC_DIR), name='static')
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(pages_router)
 
 
 @app.on_event('startup')
