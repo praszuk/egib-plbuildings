8447513e4e151fa37172c566ecda99f4deacb0aa
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-07-03 01:46:45 +0200

    Add hc data for voivodeships 02-06

--- backend/areas/data/healtcheck_all_areas_buildings.py
+++ backend/areas/data/healtcheck_all_areas_buildings.py
@@ -2,7 +2,177 @@ from backend.areas.models import HealtCheckTestAreaData
 
 
 counties = [
+    # 02 – "dolnośląskie"
+    HealtCheckTestAreaData(
+        name='głogowski',
+        teryt='0203',
+        lat=51.66531,
+        lon=16.07441,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='górowski',
+        teryt='0204',
+        lat=51.66517,
+        lon=16.54239,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='jaworski',
+        teryt='0205',
+        lat=51.05149,
+        lon=16.20572,
+        expected_tags={'building': 'office'},
+    ),
+    # 04 – "kujawsko-pomorskie"
+    HealtCheckTestAreaData(
+        name='lipnowski',
+        teryt='0408',
+        lat=52.84022,
+        lon=19.17100,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='radziejowski',
+        teryt='0411',
+        lat=52.62279,
+        lon=18.52755,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='rypiński',
+        teryt='0412',
+        lat=53.06290,
+        lon=19.41146,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='wąbrzeski',
+        teryt='0417',
+        lat=53.27565,
+        lon=18.94620,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(  # gmina Kowal
+        name='włocławski',
+        teryt='0418',
+        lat=52.53273,
+        lon=19.15530,
+        expected_tags={'building': 'office'},
+    ),
     # 06 – "lubelskie"
+    HealtCheckTestAreaData(  # gmina Konstantynów
+        name='bialski',
+        teryt='0601',
+        lat=52.20697,
+        lon=23.08980,
+        expected_tags={'building': 'yes'},  # empty
+    ),
+    HealtCheckTestAreaData(  # gmina Tarnogród
+        name='biłgorajski',
+        teryt='0602',
+        lat=50.35931,
+        lon=22.74292,
+        expected_tags={'building': 'yes'},  # empty
+    ),
+    HealtCheckTestAreaData(
+        name='hrubieszowski',
+        teryt='0604',
+        lat=50.80644,
+        lon=23.88755,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='janowski',
+        teryt='0605',
+        lat=50.70261,
+        lon=22.41879,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='krasnostawski',
+        teryt='0606',
+        lat=50.98220,
+        lon=23.16148,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='lubartowski',
+        teryt='0608',
+        lat=51.46557,
+        lon=22.60975,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='lubelski',
+        teryt='0609',
+        lat=50.44727,
+        lon=23.41521,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='łęczyński',
+        teryt='0610',
+        lat=51.29881,
+        lon=22.88610,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='łukowski',
+        teryt='0611',
+        lat=51.93003,
+        lon=22.37824,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='opolski',
+        teryt='0612',
+        lat=51.14870,
+        lon=21.97042,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='parczewski',
+        teryt='0613',
+        lat=51.63985,
+        lon=22.89702,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='puławski',
+        teryt='0614',
+        lat=51.41358,
+        lon=21.96274,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='radzyński',
+        teryt='0615',
+        lat=51.78286,
+        lon=22.61551,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='rycki',
+        teryt='0616',
+        lat=51.62444,
+        lon=21.92719,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='świdnicki',
+        teryt='0617',
+        lat=51.21957,
+        lon=22.69944,
+        expected_tags={'building': 'office'},
+    ),
+    HealtCheckTestAreaData(
+        name='tomaszowski',
+        teryt='0618',
+        lat=50.44727,
+        lon=23.41522,
+        expected_tags={'building': 'office'},
+    ),
     HealtCheckTestAreaData(
         name='włodawski',
         teryt='0619',
@@ -11,11 +181,25 @@ counties = [
         expected_tags={'building': 'office'},
     ),
     HealtCheckTestAreaData(
+        name='miasto Biała Podlaska',
+        teryt='0661',
+        lat=52.03496,
+        lon=23.12653,
+        expected_tags={'building': 'yes'},  # empty,
+    ),
+    HealtCheckTestAreaData(
         name='miasto Chełm',
         teryt='0662',
         lat=51.13321,
         lon=23.49482,
         expected_tags={'building': 'yes', 'building:levels': '4'},
+    ),
+    HealtCheckTestAreaData(
+        name='miasto Zamość',
+        teryt='0664',
+        lat=50.72284,
+        lon=23.26218,
+        expected_tags={'building': 'office'},
     ),
     # 14 – "mazowieckie"
     HealtCheckTestAreaData(
