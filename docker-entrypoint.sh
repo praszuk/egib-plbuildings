b4c536fc0c36d89b540772b73e7d4646a3bb5bfd
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-09-28 23:59:20 +0200

    Update entrypoint with alembic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Try to apply database migrations
+echo "Apply database migrations"
+until alembic -c backend/alembic.ini upgrade head ; do
+    sleep 3
+    echo "Waiting for database...Retry!";
+done
+
 # Start server
 echo "Starting server"
 uvicorn backend.main:app $( (( $DEV == 1 )) && printf %s '--reload' ) --host 0.0.0.0 --port 8000 --log-config backend/core/log_config.yaml --forwarded-allow-ips=*
