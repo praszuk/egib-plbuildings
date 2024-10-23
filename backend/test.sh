6ac5f9c6af65a6c733e37fcfe3f96a7a7f5d61e9
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-10-24 01:42:40 +0200

    Add test.sh script to run tests in docker
    Also remove test from Makefile and update workflow

---
+++ backend/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$INSIDE_DOCKER" == "1" ]; then
+  pytest -s -p no:cacheprovider "$@"
+else
+  docker compose -f docker-compose-dev.yml run -it --rm -e INSIDE_DOCKER=1 backend backend/test.sh "$@"
+fi
