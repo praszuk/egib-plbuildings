c9a4cff4bd98af90624a699de0a8f2f1e70202bb
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-10-25 01:20:44 +0200

    Add tests for db get building at

--- backend/tests/integration/test_buildings_at.py
+++ backend/tests/integration/test_buildings_at.py
@@ -4,6 +4,8 @@ import pytest
 
 from backend.areas.finder import area_finder
 from backend.services import buildings
+from backend.models.building import Building
+
 
 MOCK_AREA_TERYT_VALUE = '1421'
 
@@ -85,3 +87,75 @@ async def test_live_no_building_data(async_client, monkeypatch, test_epodgik_dat
     data = response.json()
 
     assert len(data['features']) == 0
+
+
+@pytest.mark.anyio
+async def test_db_simple_building_data(async_client, db):
+    lat1, lon1 = 52.2299575, 21.0078368
+    lat2, lon2 = 52.2300253, 21.0081468
+    lat3, lon3 = 52.2301984, 21.0080464
+    lat_search, lon_search = 52.2300062, 21.0079251
+
+    wkt_polygon = f'POLYGON(({lon1} {lat1}, {lon2} {lat2}, {lon3} {lat3}, {lon1} {lat1}))'
+
+    building = Building(
+        geometry=wkt_polygon, tags={'building': 'house', 'building:levels': 2}, teryt='123456'
+    )
+    db.add(building)
+    db.commit()
+    db.refresh(building)
+
+    response = await async_client.get(
+        'buildings/', params={'lat': lat_search, 'lon': lon_search, 'live': False}
+    )
+    assert response.status_code == 200
+
+    result = response.json()
+    feature = result['features'][0]
+    assert feature['geometry']['coordinates'][0][0] == [lon1, lat1]
+    assert feature['properties'] == {'building': 'house', 'building:levels': 2}
+
+
+@pytest.mark.anyio
+async def test_db_multiple_building_data_returns_only_one(async_client, db):
+    lat1, lon1 = 52.2299575, 21.0078368
+    lat2, lon2 = 52.2300253, 21.0081468
+    lat3, lon3 = 52.2301984, 21.0080464
+    lat_search, lon_search = 52.2300062, 21.0079251
+
+    wkt_polygon = f'POLYGON(({lon1} {lat1}, {lon2} {lat2}, {lon3} {lat3}, {lon1} {lat1}))'
+
+    building1 = Building(
+        geometry=wkt_polygon, tags={'building': 'house', 'building:levels': 2}, teryt='123456'
+    )
+    building2 = Building(
+        geometry=wkt_polygon, tags={'building': 'yes', 'building:levels': 3}, teryt='123456'
+    )
+    db.add(building1)
+    db.add(building2)
+    db.commit()
+
+    assert db.query(Building).count() == 2
+
+    response = await async_client.get(
+        'buildings/', params={'lat': lat_search, 'lon': lon_search, 'live': False}
+    )
+    assert response.status_code == 200
+
+    result = response.json()
+    assert len(result['features']) == 1
+
+
+@pytest.mark.anyio
+async def test_db_no_building_data(async_client, db):
+    lat_search, lon_search = 52.2300062, 21.0079251
+
+    assert db.query(Building).count() == 0
+
+    response = await async_client.get(
+        'buildings/', params={'lat': lat_search, 'lon': lon_search, 'live': False}
+    )
+    assert response.status_code == 200
+
+    result = response.json()
+    assert len(result['features']) == 0
