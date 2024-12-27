f9823a4208e71f399c5d50c7ff76c0577d112470
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-12-27 21:42:27 +0100

    Add test to confirm expect data in correct area

--- backend/tests/unit/test_expected_data.py
+++ backend/tests/unit/test_expected_data.py
@@ -1,3 +1,5 @@
+import pytest
+
 from backend.areas.config import all_areas
 from backend.areas.data.expected_building import (
     all_areas_data as all_dc_areas,
@@ -12,3 +14,11 @@ def test_config_areas_have_building_dc():
 
 def test_dc_area_expected_building_key_and_obj_attr_are_equal():
     assert all(teryt == area.teryt for teryt, area in all_dc_areas.items())
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+    'expected_teryt,lat,lon', [(teryt, area.lat, area.lon) for teryt, area in all_dc_areas.items()]
+)
+def test_dc_area_lat_lon_points_correct_area(area_finder, expected_teryt, lat, lon):
+    assert area_finder.area_at(lat, lon) == expected_teryt
