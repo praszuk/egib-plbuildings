ea306e93ed2b3767d4e66d63fb33e23649de564b
Paweł Raszuk <pawel.raszuk@gmail.com>
2025-01-17 18:14:31 +0100

    Change  to

--- backend/test.sh
+++ backend/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$INSIDE_DOCKER" == "1" ]; then
-  pytest -s -p no:cacheprovider "$@"
+  pytest -s -p no:cacheprovider $*
 else
-  docker compose -f docker-compose-dev.yml run -i --rm -e INSIDE_DOCKER=1 backend backend/test.sh "$@"
+  docker compose -f docker-compose-dev.yml run -i --rm -e INSIDE_DOCKER=1 backend backend/test.sh $*
 fi
